7ca6bf842149d5ef561691f3a35056abc0be7c86
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-04-04 12:24:38 -0400

    changed map layout

--- assets/js/mapbox.js
+++ assets/js/mapbox.js
@@ -1,4 +1,5 @@
 
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ3hybGx5IiwiYSI6ImNsMWkwNXUwdDA3ZzczaW0xMnRueWJlbWcifQ.9k9D-NC6Vli8SFBZ-vO2tQ';
 const map = new mapboxgl.Map({
 container: 'map', // container ID
@@ -12,6 +13,8 @@ map.addControl(
         mapboxgl: mapboxgl
     })
 );
+map.addControl(new mapboxgl.NavigationControl());
+
 // this.mapInstance = new mapboxgl.Map({
 //     container: 'map',
 //     minZoom: 0,
@@ -33,3 +36,10 @@ map.addControl(
 //     return this.defaultCenter;
 //   }
 // };
+// var apiUrl = "https://api.precisely.com/demographics-segmentation/v1/demographics/byaddress?address=2935 Broadbridge Ave, Stratford, CT&country=USA&valueFormat=PercentAsAvailable&variableLevel=Key";
+// fetch(apiUrl).then(function(response){
+//   response.json().then(function(year) {
+//     console.log(year);
+//   });
+// });
+
